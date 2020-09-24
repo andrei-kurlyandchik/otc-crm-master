410bc9186ffb00e9acad082a418be24f4f5691e2
andrey_kurliandchik <andrei.kurlyandchik@expcapital.com>
2020-09-24 23:32:15 +0300

    add "Order summer", "Orders card"

--- src/js/common.js
+++ src/js/common.js
@@ -82,6 +82,17 @@
         $('.form__select--tag').selectric('refresh');
     });
 
+    $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+        $(this)
+            .addClass("active")
+            .siblings()
+            .removeClass("active")
+            .closest("div.tabs")
+            .find("div.tabs__content")
+            .removeClass("active")
+            .eq($(this).index())
+            .addClass("active");
+    });
 }());
 
 
